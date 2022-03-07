2327687491bad8f8ee9356bd6099dbfa6ea41bf6
Angel Lopez <angellopez15luisa@gmail.com>
2022-03-07 17:11:50 -0500

    SPA control

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors=require('cors');
 const { dbConnection } = require('./database/config');
+const path = require('path');
 
 //Crear el servidor de express
 const app = express();
@@ -34,6 +35,11 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads'));
 app.use('/api/login', require('./routes/auth'));
+
+// Lo ultimo
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 
 
